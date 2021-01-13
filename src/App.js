f65ea6c4a9e1c01efbb82e9a1a0fd610cc6a51cc
Alfahmist <gst.alfahmi@gmail.com>
2021-01-13 16:35:09 +0700

    menyatukan komponen di dalamn app.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header';
 import Paper from './components/Paper';
+import TodoForm from './components/TodoForm';
+import Todos from './components/Todos'
 
 function App() {
   return (
     <Paper>
-      <section class="header">
-        <button class="header-btn main-black-color">Add</button>
-        <h1 class="header-title">Todo List</h1>
-        <button class="header-btn main-red-color">Clear</button>
-      </section>
-
-      <section class="add">
-        <form class="add-form">
-          <input type="text" class="add-input "/>
-          <button class="add-btn main-black-color">Add</button>
-        </form>
-      </section>
-
-      <section class="todos">
-        <div class="todo">
-          <span class="todo-text">Learning React</span>
-        </div>
-        <div class="todo">
-          <span class="todo-text">Learning React</span>
-        </div>
-        <div class="todo">
-          <span class="todo-text">Learning React</span>
-        </div>
-        <div class="todo">
-          <span class="todo-text">Learning React</span>
-        </div>
-      </section>
+      <Header/>
+      <TodoForm/>
+      <Todos/>
     </Paper>
   );
 }
